8d226675ae753b29f965c62df32d4fc0ec85f7ba
David Abercrombie <abercrombiedj2@gmail.com>
2022-02-25 14:51:51 +0000

    created all routes

---
+++ server/helpers/create_router.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
+
+const createRouter = function (collection) {
+
+    const router = express.Router();
+
+    // GET all users route
+    router.get('/', (req, res) => {
+        collection.find().toArray()
+        .then(docs => res.json(docs))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({status: 500, error: err});
+        });
+    });
+
+    // SHOW get user by ID
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection.findOne({_id: ObjectID(id)})
+        .then(doc => res.json(doc))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({status: 500, error: err});
+        });
+    });
+
+    // CREATE post a new user
+    router.post('/', (req, res) => {
+        const newData = req.body;
+        collection.insertOne(newData)
+        .then(result => res.json(result.ops[0]))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({status: 500, error: err});
+        });
+    });
+
+    // UPDATE put request by ID
+    router.put('/:id', (req, res) => {
+        const id = req.params.id;
+        const updateData = req.body;
+        collection.updateOne(
+            {_id: ObjectID(id)},
+            {$set: updatedData}
+        )
+        .then(result => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({status: 500, error: err});
+        });
+    });
+
+    // DESTROY delete request by ID
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+        collection.deleteOne({_id: ObjectID(id)})
+        .then(result => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({status: 500, error: err});
+        });
+    })
+
+    return router;
+
+};
+
+module.exports = createRouter;
