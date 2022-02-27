0839ee79dfa329a12f7e4f7c277df20cd63c077d
Christopher Mckenna <christopher.mckenna123@gmail.com>
2022-02-27 20:17:38 +0000

    charts working

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,52 +1,11 @@
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
+import UserChart from "./UserChart";
 
 const UserCard = ({user, updateUser, deleteUser}) => {
 
-   const nameOfUser = user.userName
-   const travelData = user.travel
-   const foodData = user.food
-   const homeData = user.home
-
-
-    const options = {
-        chart: {
-          renderTo: 'container',
-          type: 'pie'
-      },
-      title: {
-          text: 'Emissions'
-      },
-      yAxis: {
-          title: {
-              text: 'User Emissions'
-          }
-      },
-      
-      series: [{
-          type: 'pie',
-          name: {nameOfUser},
-          data: [{
-              name: 'Travel',
-              y: {travelData}
-            },
-            {
-              name: 'Food',
-              y: {foodData}
-            },
-            {
-              name: 'Home',
-              y: {homeData}
-            }
-          ]
-      }]
-      };
 
     const handleDeleteUser = () => {
         deleteUser(user._id);
     }
-
-    
 
     return (
         <div className='user-card'>
@@ -55,7 +14,7 @@ const UserCard = ({user, updateUser, deleteUser}) => {
             <p>{user.food}</p>
             <p>{user.home}</p>
             <button onClick={handleDeleteUser}> 🗑 </button>
-            <HighchartsReact highcharts={Highcharts} options={options} />
+            <UserChart user={user} />
         </div>
     )
 };
---
+++ client/src/components/UserChart.js
@@ -0,0 +1,53 @@
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+
+const UserChart = ({user}) => {
+
+
+const pieName = user.userName;
+const pieTravel = user.travel;
+const pieFood = user.food;
+const pieHome = user.home;
+
+ const options = {
+     chart: {
+       renderTo: 'container',
+       type: 'pie'
+   },
+   title: {
+       text: 'Emissions'
+   },
+   yAxis: {
+       title: {
+           text: 'User Emissions'
+       }
+   },
+   
+   series: [{
+       type: 'pie',
+       name: pieName,
+       data: [{
+           name: 'Travel',
+           y: pieTravel
+         },
+         {
+           name: 'Food',
+           y: pieFood
+         },
+         {
+           name: 'Home',
+           y: pieHome
+         }
+       ]
+   }]
+   };
+
+   return (
+       <div className="user-chart">
+           <HighchartsReact highcharts={Highcharts} options={options} />
+       </div>
+   );
+}
+
+export default UserChart;
