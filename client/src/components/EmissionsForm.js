d3c65e20f6f0c8ade92245f51bccd3caaacdc799
David Abercrombie <abercrombiedj2@gmail.com>
2022-02-28 12:28:55 +0000

    updated form inputs and functionality

--- client/src/components/EmissionsForm.js
+++ client/src/components/EmissionsForm.js
@@ -4,27 +4,63 @@ import "../stylesheets/EmissionsForm.css"
 const EmissionsForm = ({addUser}) => {
 
     const [userName, setUserName] = useState('');
-    const [travel, setTravel] = useState(0);
+    const [landTravel, setLandTravel] = useState(0);
+    const [airTravel, setAirTravel] = useState(0);
     const [food, setFood] = useState(0);
-    const [home, setHome] = useState(0);
+    const [lifestyle, setLifestyle] = useState(0);
+    const [electricity, setElectricity] = useState(0);
+    const [gas, setGas] = useState(0);
+    const [total, setTotal] = useState(0);
 
     const handleUserNameChange = event => setUserName(event.target.value);
-    const handleFoodChange = event => setFood(event.target.value);
-    const handleHomeChange = event => setHome(event.target.value);
-    const handleTravelChange = event => setTravel(parseInt(event.target.value));
+
+    const handleLandTravelChange = event => setLandTravel(parseInt(event.target.value));
+
+    const handleAirTravelChange = event => {
+        const airTravelEmissions = parseInt(event.target.value) * 2;
+        setAirTravel(airTravelEmissions);
+    };
+
+    const handleFoodChange = event => {
+        const foodEmissions = parseInt(event.target.value) * 0.4;
+        setFood(foodEmissions);
+    };
+
+    const handleLifestyleChange = event => {
+        const lifestyleEmissions = parseInt(event.target.value);
+        setLifestyle(lifestyleEmissions);
+    };
+
+    const handleElectricityChange = event => {
+        const electricityEmissions = parseInt(event.target.value);
+        setElectricity(electricityEmissions);
+    };
+    
+    const handleGasChange = event => {
+        const gasEmissions = parseInt(event.target.value);
+        setGas(gasEmissions);
+    };
 
     const handleSubmit = event => {
         event.preventDefault();
+        const totalEmissions = landTravel + airTravel + food + lifestyle + electricity + gas;
+        setTotal(totalEmissions);
         addUser({
             userName: userName,
-            travel: travel,
+            landTravel: landTravel,
+            airTravel: airTravel,
             food: food,
-            home: home
+            lifestyle: lifestyle,
+            electricity: electricity,
+            gas: gas
         });
         setUserName('');
-        setTravel(0);
+        setLandTravel(0);
+        setAirTravel(0);
         setFood(0);
-        setHome(0);
+        setLifestyle(0);
+        setElectricity(0);
+        setGas(0);
     };
 
     return (
@@ -43,11 +79,11 @@ const EmissionsForm = ({addUser}) => {
                     onChange={handleUserNameChange} />
                 </div>
                 <div className='form-input'>
+                    <label htmlFor='land-travel'>How do you normally travel?</label>
                     <select
                     name='land-travel'
                     id='land-travel'
-                    onChange={handleTravelChange}>
-                    <option value=''>How do you normally travel?</option>
+                    onChange={handleLandTravelChange}>
                     <option value='4'>Car</option>
                     <option value='3'>Motorbike</option>
                     <option value='1'>Train</option>
@@ -59,24 +95,53 @@ const EmissionsForm = ({addUser}) => {
                     </select>
                 </div>
                 <div className='form-input'>
-                    <label htmlFor='food'>Food:  </label>
+                    <label htmlFor='air-travel'>How many return flights have you taken in the last twelve months?</label>
+                    <input
+                    type='number'
+                    id='air-travel'
+                    name='air-travel'
+                    required
+                    onChange={handleAirTravelChange} />
+                </div>
+                <div className='form-input'>
+                    <label htmlFor='food'>How many meals per week do you eat containing meat?</label>
                     <input
                     type='number'
                     id='food'
                     name='food'
-                    value={food}
                     required
                     onChange={handleFoodChange} />
                 </div>
                 <div className='form-input'>
-                    <label htmlFor='home'>Home: </label>
-                    <input
-                    type='number'
-                    id='home'
-                    name='home'
-                    value={home}
-                    required
-                    onChange={handleHomeChange} />
+                    <label htmlFor='lifestyle'>How much do you spend on clothes, health and beauty products each month?</label>
+                    <select
+                    name='lifestyle'
+                    id='lifestyle'
+                    onChange={handleLifestyleChange}>
+                    <option value='1'>Less than £50</option>
+                    <option value='2'>Less than £100</option>
+                    <option value='3'>More than £100</option>
+                    </select>
+                </div>
+                <div className='form-input'>
+                    <label htmlFor='electricity'>Is your electricity from renewable sources?</label>
+                    <select
+                    name='electricity'
+                    id='electricity'
+                    onChange={handleElectricityChange}>
+                    <option value='0'>Yes</option>
+                    <option value='1'>No</option>
+                    </select>
+                </div>
+                <div className='form-input'>
+                    <label htmlFor='gas'>Is your gas carbon neutral?</label>
+                    <select
+                    name='gas'
+                    id='gas'
+                    onChange={handleGasChange}>
+                    <option value='0'>Yes</option>
+                    <option value='1'>No</option>
+                    </select>
                 </div>
                 <input
                 type='submit'
@@ -84,6 +149,7 @@ const EmissionsForm = ({addUser}) => {
                 value='Save' />
             </form>
         </div>
+        <h3>Your carbon footprint is: {total} tonnes.</h3>
         </div>
     )
 };
