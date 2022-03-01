a482362dc69a052be2827161278f66b1bd277d69
Paolaguerralibrero <mpaolamcra@hotmail.com>
2022-03-01 14:55:47 +0000

    learnmorecss

--- client/src/components/LearnMore.js
+++ client/src/components/LearnMore.js
@@ -1,4 +1,4 @@
-
+import '../stylesheets/LearnMore.css'
 
 const LearnMore = () => {
 
@@ -6,8 +6,9 @@ const LearnMore = () => {
     return (
 
         <div className= 'learn-more'>
-            <h1>What can we do to reduce our CO2 footprint?</h1>
-            <h2>Food</h2>
+            <h1 className='learn-moreh1'>What can we do to reduce our CO2 footprint?</h1>
+            <div className='learn-more1'>
+            <h2 className='learn-moreh2'>Food</h2>
             <ul>
                 <li>Consume local and seasonal products (forget strawberries in winter) </li>
                 <li>Limit meat consumption, especially beef  </li>
@@ -15,24 +16,31 @@ const LearnMore = () => {
                 <li>Bring reusable shopping bags and avoid products with excessive plastic packaging </li>
                 <li>Make sure to buy only what you need, to avoid waste</li>
             </ul>
-            <h2>Clothing</h2>
+            </div>
+            <div className='learn-more1'>
+            <h2 className='learn-moreh2'>Clothing</h2>
             <ul>
                 <li>Take good care of your clothes </li>
                 <li>Try swapping, borrowing, renting or buying second-hand  </li>
                 <li>Buy responsibly-made clothes, e.g. made from recycled material or with an eco-label  </li>
             </ul>
-            <h2>Transport</h2>
+            </div>
+            <div className='learn-more1'>
+            <h2 className='learn-moreh2'>Transport</h2>
             <ul>
                 <li>Cycle or use public transport </li>
                 <li>Be smart about when and how you drive  </li>
                 <li>Try the train for your next holiday  </li>
             </ul>
-            <h2>Energy and waste</h2>
+            </div>
+            <div className='learn-more1'>
+            <h2 className='learn-moreh2'>Energy and waste</h2>
             <ul>
                 <li>Turn down the heating by 1°, it will already make a difference </li>
                 <li>Take short showers   </li>
                 <li>Limit and recycle your waste  </li>
             </ul>
+            </div>
             
 
 
