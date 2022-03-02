a926b2f628da6ef73003665eca6b7acbdea5432d
Davebgla <91500089+Davebgla@users.noreply.github.com>
2022-03-02 19:20:04 +0000

    landing page new feature

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -6,18 +6,21 @@ const LandingPage = () => {
 
 return (
     <div className='landing-page'>
-        <h2 className='statement'>The way we go about our daily lives can drastically<br/>
-        impact the environment we share. Use the E.mission<br/> calculator to discover
-        your own carbon footprint,<br/> compare it with others and get advice on how it can<br/> be reduced
-        to combat global emissions.
-        </h2>
+        <div className='landing-logo'>
+        <h3 className='logo'><span id='e'>E.</span>mission</h3>
+        </div>
         <blockquote className='landing-paragraph'>
         <p>"Never before have we had such an awareness of what we are doing to the planet,<br/>
-        and never before have we had the power to do something about that."</p>
+        and never before have we had the power to do something about that."<br/>--Sir David Attenborough </p>
         </blockquote>
-        <figcaption className='sird'> --Sir David Attenborough </figcaption> 
+        <div className='container'>
+        <h2 className='statement'>The way we go about our daily lives can drastically<br/>
+        impact the environment we share. Use the E.mission<br/> calculator to discover
+        your own carbon footprint.
+        </h2>
         <div className='button-div'>
         <button className="landing-page-button"><a className="button-link" href="/emissionsform">Get Started</a></button>
+        </div>
         </div>
         {/* <div className='counter-div'>
             <p>
